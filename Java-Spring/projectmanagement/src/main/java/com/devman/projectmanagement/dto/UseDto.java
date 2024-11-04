47f2a8d31d423805a7dca5b6dc458a7f18e335de
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-04 15:32:41 +0330

    main : spring, closed Java spring, unfinished task

--- Java-Spring/projectmanagement/src/main/java/com/devman/projectmanagement/dto/UseDto.java
+++ Java-Spring/projectmanagement/src/main/java/com/devman/projectmanagement/dto/UseDto.java
@@ -1,2 +1,22 @@
-package com.devman.projectmanagement.dto;public class UseDto {
+package com.devman.projectmanagement.dto;
+
+public class UseDto {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
