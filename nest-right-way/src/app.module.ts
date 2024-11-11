6b328eab6af55ce89f808e6f5ba33ca372368f9f
Ratovonirina <eric.ratovonirina@gmail.com>
2024-11-11 15:30:08 +0330

    main : nest, S in SOLID example

--- nest-right-way/src/app.module.ts
+++ nest-right-way/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './srp/order/order.module';
+import { ProductModule } from './srp/product/product.module';
 
 @Module({
-  imports: [],
+  imports: [OrderModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
